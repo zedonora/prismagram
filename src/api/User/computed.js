b1def18d271739e6515af0d61e398ce5a461ffa4
zedonora <timeundtime@gmail.com>
2019-12-26 06:01:03 +0900

    fragment refactoring

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,24 +5,28 @@ export default {
         fullName: (parent) => {
             return `${parent.firstName} ${parent.lastName}`;
         },
-        amIFffollowing: async(parent, _, {request}) => {
+        isFollowing: async(parent, _, {request}) => {
             const { user } = request;
             const { id:parentId } = parent;
             try {
-                const exists = await prisma.$exists.user({AND: [{id:parentId}, {followers_some: [user.id] }] });
-                console.log(exists);
-                if (exists) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return prisma.$exists.user({
+                    AND: [
+                        {
+                            id: user.id
+                        }, 
+                        {
+                            following_some: {
+                                id: parentId
+                            } 
+                        }
+                    ] 
+                });
             } catch (error) {
                 console.log(error);
                 return false;
             }
-
         },
-        itsMe: (parent, _, {request}) => {
+        isSelf: (parent, _, {request}) => {
             const {user} = request;
             const { id: parentId } = parent;
             return user.id === parentId;
--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,34 @@
 export const COMMENT_FRAGMENT= `
-    fragment CommentPargs on Comment{
-        id
-        text
-        user {
-            username
-        }
+    id
+    text
+    user {
+        username
     }
 `;  
+
+export const USER_FRAGMENT = `
+    id
+    username
+`
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`
