c250bf609f14ceecb7c03ce666ea4f583bf3ec24
zedonora <timeundtime@gmail.com>
2020-01-03 05:13:41 +0900

    login job

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,16 +1,20 @@
 import { prisma } from "../../../../generated/prisma-client";
 export default {
-  Mutation: {
-    createAccount: async (_, args) => {
-      const { username, email, firstName="", lastName="", bio="" } = args;
-      const user = await prisma.createUser({
-        username,
-        email,
-        firstName,
-        lastName,
-        bio
-      });
-      return user;
-    }
-  }
+	Mutation: {
+		createAccount: async (_, args) => {
+			const { username, email, firstName = "", lastName = "", bio = "" } = args;
+			const exists = await prisma.$exists.user({ username });
+			if (exists) {
+				throw Error("This username is already taken");
+			}
+			await prisma.createUser({
+				username,
+				email,
+				firstName,
+				lastName,
+				bio
+			});
+			return true;
+		}
+	}
 };
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -2,17 +2,17 @@ import { generateSecret, sendSecretMail } from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-  Mutation: {
-    requestSecret: async (_, args) => {
-      const { email } = args;
-      const loginSecret = generateSecret();
-      try {
-        await sendSecretMail(email, loginSecret);
-        await prisma.updateUser({ data: { loginSecret }, where: { email } });
-        return true;
-      } catch {
-        return false;
-      }
-    }
-  }
+	Mutation: {
+		requestSecret: async (_, args) => {
+			const { email } = args;
+			const loginSecret = generateSecret();
+			try {
+				await sendSecretMail(email, loginSecret);
+				await prisma.updateUser({ data: { loginSecret }, where: { email } });
+				return true;
+			} catch {
+				return false;
+			}
+		}
+	}
 };
