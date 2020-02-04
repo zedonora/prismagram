e60d659f5d12e687abca66977e9072f45c539db7
zedonora <timeundtime@gmail.com>
2020-02-05 05:11:53 +0900

    multer

--- src/server.js
+++ src/server.js
@@ -5,17 +5,19 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-  schema,
-  context: ({ request }) => ({ request, isAuthenticated })
+	schema,
+	context: ({ request }) => ({ request, isAuthenticated })
 });
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
-  console.log(`✅ Server running on http://localhost:${PORT}`)
+	console.log(`✅ Server running on http://localhost:${PORT}`)
 );
