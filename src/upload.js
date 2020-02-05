edc967ee39df4823cf11d64a244e7aaa06c4ca0e
zedonora <timeundtime@gmail.com>
2020-02-06 05:17:40 +0900

    midifired

--- src/upload.js
+++ src/upload.js
@@ -1,5 +1,31 @@
 import multer from "multer";
+//import multerS3 from "multer-s3";
+//import aws from "aws-sdk";
 
+// const s3 = new aws.S3({
+// 	accessKeyId: process.env.AWS_KEY,
+// 	secretAccessKey: process.env.AWS_SECRET,
+// 	reguion: "ap-northeast-2"
+// });
+
+// const upload = multer({
+// 	storage: multerS3({
+// 		s3: s3,
+// 		bucket: "prismagram",
+// 		metadata: function(req, file, cb) {
+// 			cb(null, { fieldName: file.fieldName });
+// 		},
+// 		key: function(req, file, cb) {
+// 			cb(null, Date.now().toString());
+// 		}
+// 	})
+// });
+// export const uploadController = (req, res) => {
+// 	const {
+// 		file: { location }
+// 	} = req;
+// 	res.json({ location });
+// };
 const upload = multer({ dest: "uploads/" });
 
 export const uploadMiddleware = upload.single("file");
