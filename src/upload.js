6ecd60f7b8a661abe3b6c1b265f18ea5ce2ce0c6
zedonora <timeundtime@gmail.com>
2020-02-10 09:29:45 +0900

    upload

--- src/upload.js
+++ src/upload.js
@@ -1,38 +1,42 @@
 import multer from "multer";
-//import multerS3 from "multer-s3";
-//import aws from "aws-sdk";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-// const s3 = new aws.S3({
-// 	accessKeyId: process.env.AWS_KEY,
-// 	secretAccessKey: process.env.AWS_SECRET,
-// 	reguion: "ap-northeast-2"
-// });
+const s3 = new aws.S3({
+	accessKeyId: process.env.AWS_KEY,
+	secretAccessKey: process.env.AWS_SECRET,
+	region: "ap-northeast-2"
+});
 
-// const upload = multer({
-// 	storage: multerS3({
-// 		s3: s3,
-// 		bucket: "prismagram",
-// 		metadata: function(req, file, cb) {
-// 			cb(null, { fieldName: file.fieldName });
-// 		},
-// 		key: function(req, file, cb) {
-// 			cb(null, Date.now().toString());
-// 		}
-// 	})
-// });
-// export const uploadController = (req, res) => {
-// 	const {
-// 		file: { location }
-// 	} = req;
-// 	res.json({ location });
-// };
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+	storage: multerS3({
+		s3,
+		acl: "public-read",
+		bucket: "prismagram-yg",
+		metadata: function(req, file, cb) {
+			cb(null, { fieldName: file.fieldname });
+		},
+		key: function(req, file, cb) {
+			cb(null, Date.now().toString());
+		}
+	})
+});
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
 	const {
-		file: { path }
+		file: { location }
 	} = req;
-	res.json({ path });
+	res.json({ location });
 };
+//const upload = multer({ dest: "uploads/" });
+
+//export const uploadMiddleware = upload.single("file");
+
+//export const uploadController = (req, res) => {
+//	const {
+//		file: { path }
+//	} = req;
+//	res.json({ path });
+//};
